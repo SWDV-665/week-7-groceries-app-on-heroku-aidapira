8655a9b2baca0c7eb39240bbd92dd3587f6ed725
Aida <aidapirah@gmail.com>
2024-07-15 13:34:40 -0700

    Add remove and add item features

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,62 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title = 'Grocery';
 
+  items = [
+    {
+      name: 'Milk',
+      quantity: 2
+    },
+    {
+      name: 'Bread',
+      quantity: 1
+    },
+    {
+      name: 'Banana',
+      quantity: 3
+    },
+    {
+      name: 'Sugar',
+      quantity: 1
+    },
+  ]
+  constructor(public toastCtrl: ToastController) { }
+
+  async removeItem(item: any, index: any) {
+    console.log("Removing item -- ", item)
+    const toast = await this.toastCtrl.create({
+      message: "Removing item - " + item.name + "...",
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1)
+  }
+
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Cancel clicked');
+      }
+    },
+    {
+      text: 'Save',
+      handler: (item: any) => {
+        this.items.push(item)
+      }
+    }];
+  public alertInputs = [
+    {
+      name: 'name',
+      placeholder: 'Name',
+    },
+    {
+      name: 'quantity',
+      type: 'number',
+      placeholder: 'Quantity',
+    },
+  ];
 }
